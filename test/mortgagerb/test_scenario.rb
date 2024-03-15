04bbe344ec0cad45bc341a5f3b154ae53afadf2f
Jason Kim <iamjsonkim@gmail.com>
2024-03-15 00:55:53 -0700

    test working for test_calculate_thirty_year_fixed_get_pricipal

--- test/mortgagerb/test_scenario.rb
+++ test/mortgagerb/test_scenario.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class TestMortgagerbScenario < Minitest::Test
   ANNUAL_RATE = 5.5
   YEARS = 30
-  PRINCIPAL = 1000000
+  PRINCIPAL = 1_000_000
   TYPE = :thirty_year_fixed
 
   def test_initialize
@@ -11,5 +11,11 @@ class TestMortgagerbScenario < Minitest::Test
     amortization_payment = scenario.calculate
 
     assert amortization_payment == 5677.89
+  end
+
+  def test_calculate_thirty_year_fixed_get_pricipal
+    scenario = ::Mortgagerb::Scenario.new(5677.89, ANNUAL_RATE, :thirty_year_fixed_get_pricipal)
+    principal = scenario.calculate
+    assert principal.round == PRINCIPAL
   end
 end
