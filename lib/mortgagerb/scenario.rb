9ee7566956d38823a28f8105c0d7ba1b45c91972
Jason Kim <iamjsonkim@gmail.com>
2024-03-15 00:42:02 -0700

    include thirty_year_fixed_get_pricipal

--- lib/mortgagerb/scenario.rb
+++ lib/mortgagerb/scenario.rb
@@ -1,17 +1,22 @@
 module Mortgagerb
   class Scenario
-    def initialize(principal, rate, type)
-      @principal = principal
+    def initialize(amount, rate, type)
+      @amount = amount
       @rate = rate
       @type = type
       @amortization_payment = nil
+      @principal = nil
     end
 
     def calculate
       if @type == :thirty_year_fixed
         r = Calculator.periodic_interest_rate(@rate)
         n = Calculator.total_number_of_payments(30)
-        @amortization_payment = Calculator.periodic_amortization_payment(@principal, r, n)
+        @amortization_payment = Calculator.periodic_amortization_payment(@amount, r, n)
+      elsif @type == :thirty_year_fixed_from_amortization
+        r = Calculator.periodic_interest_rate(@rate)
+        n = Calculator.total_number_of_payments(30)
+        @principal = Calculator.principal_from_amortization_payment(@amount, r, n)
       end
     end
   end
